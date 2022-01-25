b9b7775e1483c0f81d9b629ab5d4372545a85060
Guantingdraw <91120414+Guantingdraw@users.noreply.github.com>
2022-01-25 15:48:18 +0800

    Add files via upload

---
+++ build/MotorUnion/CMakeFiles/MotorUnion.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/MotorUnion/MotorUnion.cpp" "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/CMakeFiles/MotorUnion.dir/MotorUnion.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MotorUnion_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/CMakeFiles/Motor.dir/DependInfo.cmake"
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/dynamixel/CMakeFiles/DynamixelSdk.dir/DependInfo.cmake"
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/motor/CMakeFiles/motor.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/MotorUnion/Motor/CMakeFiles/Motor.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/MotorUnion/Motor/Motor.cpp" "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/CMakeFiles/Motor.dir/Motor.cpp.o"
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/MotorUnion/Motor/MotorMx.cpp" "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/CMakeFiles/Motor.dir/MotorMx.cpp.o"
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/MotorUnion/Motor/MotorPro.cpp" "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/CMakeFiles/Motor.dir/MotorPro.cpp.o"
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/MotorUnion/Motor/MotorProPlus.cpp" "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/CMakeFiles/Motor.dir/MotorProPlus.cpp.o"
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/MotorUnion/Motor/MotorXm.cpp" "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/CMakeFiles/Motor.dir/MotorXm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "Motor_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/dynamixel/CMakeFiles/DynamixelSdk.dir/DependInfo.cmake"
+  "/home/jason-nano/I_want_to_fly-vel_plan_humanoid_new/Motor_Driver/src/build/MotorUnion/Motor/motor/CMakeFiles/motor.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
